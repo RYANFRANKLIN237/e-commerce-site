1ae09c3463ed1604787cdc4577a71e4ba2daba89
RYANFRANKLIN SUMBELE <Ngometune@gmail.com>
2024-12-13 20:53:34 +0100

    products

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -26,44 +26,42 @@ class CategoryController extends Controller
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+
     public function store(Request $request)
     {
         $this->category->create($request->all());
         return redirect()->back();
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(string $id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+
     public function edit(string $id)
     {
         //
+        $response['category'] = $this->category->find($id);
+        return view('pages.category.edit')->with($response);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+
     public function update(Request $request, string $id)
     {
         //
+        $category = $this->category->find($id);
+        $category->update(array_merge($category->toArray(), $request->toArray()));
+        return redirect('category');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+    
     public function destroy(string $id)
     {
         //
+        $category = $this->category->find($id);
+        $category->delete();
+        return redirect('category');
     }
 }
