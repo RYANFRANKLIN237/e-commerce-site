ca44632ccfaa32c9a50dd5749010432fa169c443
RYANFRANKLIN SUMBELE <Ngometune@gmail.com>
2024-12-20 23:02:02 +0100

    multi user authentication

---
+++ app/Http/Controllers/Admin/AdminRegistationController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AdminRegistationController extends Controller
+{
+    public function create()
+    {
+        return view('admin.create');
+    }
+    public function store(Request $request)
+    {
+       $input = $request->all();
+       User::create([
+        'name' => $input['name'],
+        'email' => $input['email'],
+        'password' => Hash::make($input['password']),
+        'role' => 'admin'
+
+      ]);
+       return view('admin.thank');
+    }
+}
